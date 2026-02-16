fc050ec3b7875060c8409eb30c55a7a3e2c1aeda
mreddyveera <reddyveeramanikanta54304@gmail.com>
2026-02-16 19:21:49 +0530

    Variables

---
+++ 02_variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Mani: Hi Trinadh"
+echo "Trinadh: Hi Mani"
+echo "Mani: How are you?"
+echo "Trinadh: I am fine. Thank you"
---
+++ 03_variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1="Mani"
+PERSON2="Trinadh"
+echo "$PERSON1: Hi $PERSON2"
+echo "$PERSON2: Hi $PERSON1"
+echo "$PERSON1: How are you?"
+echo "$PERSON2: I am fine. Thank you"
