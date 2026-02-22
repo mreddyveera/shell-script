c24a18506f3c7a93333f7f35c3f49f8d9320a2f2
mreddyveera <reddyveeramanikanta54304@gmail.com>
2026-02-22 16:53:27 +0530

    Installing a script

---
+++ 10_installscripts.sh
@@ -0,0 +1,29 @@
+#!bin/bash
+
+USER=$(id -u)
+
+if [ $USER -ne 0 ]
+then
+    echo "ERROR::Please switch to the root user"
+    exit 1
+fi
+
+dnf install mysql -y
+
+if [ ($!) -ne 0 ]
+then
+    echo "ERROR:: Installing mysql ....Failed"
+    exit1
+else
+    echo "Error:: Mysql....success"
+fi
+
+dnf install git -y
+
+if [ ($!) -ne 0 ]
+then
+    echo "ERROR:: Installing git ... FAILURE"
+    exit 1
+else
+    echo "Installing Git ... SUCCESS"
+fi
